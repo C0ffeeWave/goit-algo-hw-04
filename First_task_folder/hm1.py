b3adf5f9c2cd2ede04bcbb27175dc96ae3f8e5ba
Wave <ert4.51c@gmail.com>
2024-04-26 20:12:22 +0300

    change

---
+++ First_task_folder/hm1.py
@@ -0,0 +1,3 @@
+with open('D:\Projects\my_repo\goit-algo-hw-04\goit-algo-hw-04\goit-algo-hw-04\First_task_folder/salary.txt', 'r', encoding='utf-8') as fh:
+    content = fh.read()
+    print(content)
